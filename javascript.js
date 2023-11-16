a3079f299aba3bf47dca4e88c2a1aec77b757514
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2023-11-15 20:11:47 -0700

    Created a function that outputs the order object for printing to the console.

--- javascript.js
+++ javascript.js
@@ -7,17 +7,20 @@ const userInput = prompt("Please enter your froyo flavors as a list, separating 
 // 2. Check each flavor for multiples and store them in an object "order"
 const order = {};
 
-// 3. Loop to obtain the elements of the userInput array and check for multiples.
-for (let i = 0; i < userInput.length; i++){
-  //If-else checks each element of userInput to see if it is undefined (it doesn't exist as a key in the order object).
-  if (order[userInput[i]] === undefined){
-    order[userInput[i]] = 1;//If true, then add the element as a key in the order object.
-  }else{
-    order[userInput[i]]++;//If false, increment the value of this key.
+/* 3. Create a function that loops to obtain the elements 
+      of the userInput array and check for multiples.*/
+function countFlavors(orders){
+  for (let i = 0; i < userInput.length; i++){
+    //If-else checks each element of userInput to see if it is undefined (it doesn't exist as a key in the order object).
+    if (orders[userInput[i]] === undefined){
+      orders[userInput[i]] = 1;//If true, then add the element as a key in the order object.
+    }else{
+      orders[userInput[i]]++;//If false, increment the value of this key.
+    }
   }
+  return orders;
 }
-
-// 4. Print the user's order in a table format:
-console.table(order);
+// 4. Call the function and print the user's order in a table format:
+console.table(countFlavors(order));
 
 /**** End of Program ****/
